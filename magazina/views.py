685be3307d286db4944e3bbad5a4c9c80f65cca7
Geraldo <gerdomi44@gmail.com>
2026-09-25 07:54:11 +0200

    Ndrysho URL e QR code te menaxho_produktin

--- magazina/views.py
+++ magazina/views.py
@@ -740,9 +740,7 @@ def gjenero_qr(request, produkt_id):
     produkti = get_object_or_404(produktet_e_perdoruesit(request.user), id=produkt_id)
 
     # Ndërtojmë linkun e plotë për te faqja 'menaxho_produktin'
-    url_menaxho = request.build_absolute_uri(
-        reverse('menaxho_produktin', kwargs={'produkt_id': produkti.id})
-    )
+    url = request.build_absolute_uri(reverse('menaxho_produktin', args=[produkti.id]))
 
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
     qr.add_data(url_menaxho)
